d77f4f7e4da2c4f86906a7a535c6dd376fc0ab07
Jacob Hocker <jacobdouglas06@gmail.com>
2023-05-24 13:37:06 -0500

    Styling the neon text and input bar.  Trouble shooting styles and colors on deployed version for Klotski.

--- src/games/klotski/components/ImgInput.js
+++ src/games/klotski/components/ImgInput.js
@@ -1,4 +1,4 @@
-import React, {  useContext, useEffect } from 'react';
+import React, {  useState, useContext, useEffect } from 'react';
 import { KlotskiContext } from '../Klotski';
 import { updateUrlParams } from '../helpers/helpers';
 import '../styles/ImgInput.css';
@@ -6,6 +6,7 @@ import { randomImg } from '../helpers/imgs';
 
 const ImgInput = () => {
     const { img, setImg, shuffleTiles, setIsStarted, setMoves } = useContext(KlotskiContext);
+    const [showInput, setShowInput] = useState(false);
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search)
@@ -15,8 +16,10 @@ const ImgInput = () => {
     }, [])
 
     const handleImgChange = (e) => {
-        setImg(e.target.value)
-        window.history.replaceState("", "", updateUrlParams(window.location.href, "img", img))
+        setImg(e.target.value);
+        
+        window.history.replaceState("", "", updateUrlParams(window.location.href, "img", img));
+        
     };
     
     const handleRandomImageClick = () => {
@@ -27,16 +30,46 @@ const ImgInput = () => {
     };
 
     return (
-        <div className='klotskiImgInput'>
-            <button onClick={handleRandomImageClick}>
-                Random Image
-            </button>
-            <label>Copy & Paste Sqaure Image Link </label>
-            <input 
-            value={img} 
-            onChange={handleImgChange} 
-            className='klotskiInput'
-            />
+        <div className='klotskiInputSection'>
+
+            { !showInput ?
+            <div className='klotskiInputBtnContainer'>
+                <button onClick={handleRandomImageClick} className='klotskiRndImg'>
+                    <span>
+                        Random 
+                    </span>
+                    <span>
+                        Image
+                    </span>
+                </button>
+                <button onClick={() => setShowInput(true)} className='klotskiCustImg'>
+                    <span>
+                        Custom 
+                    </span>
+                    <span>
+                        Image
+                    </span>
+                </button>
+            </div>
+            :
+            <div className='klotskiCustomInput'>
+                <label>Copy & Paste Sqaure Image Link </label>
+                <div className='klotskiInputContainer'>
+                    <input 
+                    value={img} 
+                    onChange={handleImgChange} 
+                    className='klotskiInput'
+                    />
+                    <button onClick={() => {setShowInput(false)}}
+                    className='klotskiCloseImg'
+                    >
+                        X
+                    </button>
+                </div>
+                
+            </div>
+            
+            }
             
         </div>
     )
