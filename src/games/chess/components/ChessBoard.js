a75c082b860eee16b8f0e4d42979aab62822acc7
Jacob Hocker <jacobdouglas06@gmail.com>
2023-05-30 12:47:26 -0500

    fixed deployment issues.

--- src/games/chess/components/ChessBoard.js
+++ src/games/chess/components/ChessBoard.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/ChessBoard.css';
 import Tile from './Tile';
+
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+
+
+
+
 
 
 
@@ -10,7 +18,7 @@ const ChessBoard = () => {
     for (let j = verticalAxis.length - 1; j >= 0; j--) {
         for (let i = 0; i < horizontalAxis.length; i++) {
             const number = j + i + 2;
-            board.push(<Tile key={i + 10} img={img} number={number} />)
+            board.push(<Tile img={img} number={number} />)
         }
     }
 
