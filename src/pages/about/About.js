9cd22a9c73c427bce80b4ba1a529b8ca9cd4b2d1
Jacob Hocker <jacobdouglas06@gmail.com>
2023-10-26 11:36:12 -0400

    added the base template and began fleshing out the about page.

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -3,7 +3,37 @@ import './About.css';
 
 const About = () => {
     return (
-        <div>About</div>
+        <section className='aboutPage'>
+            <div className='aboutHeader'>
+                <h1>About Pixel Palace</h1>
+            </div>
+            <div className='aboutContent'>
+                <h1>Built With</h1>
+
+
+                <div className='aboutBuiltWith'>
+                    <h2>Tetris</h2>
+                    <div className='builtWithList'>
+                        <p>React</p>
+                        <p>Styled Components</p>
+                        <p>CSS</p>
+                    </div>
+                </div>
+
+                <div className='aboutBuiltWith'>
+                    <h2>Wordle</h2>
+                    <div className='builtWithList'>
+                        <p>React</p>
+                        <p>Styled Components</p>
+                        <p>CSS</p>
+                    </div>
+                </div>
+
+
+
+            </div>
+
+        </section>
     )
 }
 
